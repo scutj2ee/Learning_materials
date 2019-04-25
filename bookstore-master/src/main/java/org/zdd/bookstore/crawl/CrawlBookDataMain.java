7f005e2e15b59bb75ca57228b16ae00671113fb6
1145054472@qq.com <1145054472@qq.com>
2019-04-25 22:18:27 +0800

    上传学习资料

---
+++ bookstore-master/src/main/java/org/zdd/bookstore/crawl/CrawlBookDataMain.java
@@ -0,0 +1,23 @@
+package org.zdd.bookstore.crawl;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.zdd.bookstore.model.entity.BookInfo;
+
+import java.util.List;
+
+public class CrawlBookDataMain {
+
+    public static void main(String[] args) throws Exception {
+
+        //创建HttpClient
+        HttpClient httpclient = new DefaultHttpClient();
+        //先去书籍列表页列表页
+        //种子
+        String url = "http://category.dangdang.com/cp01.54.06.00.00.00.html";
+        //通过URLEntity获取实体中的信息
+        List<BookInfo> books = URLEntity.URLParse(httpclient, url,"7");
+        //mysql_control.executeInsert(books);  //数据库添加数据
+        new WriteToMysql().executeInsert(books);
+    }
+}
